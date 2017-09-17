27fb882633b16b8328c359a6c56c1a8725265001
ThinkerM <filip.milichovsky@gmail.com>
2017-09-17 10:33:33 +0200

    Removed unnecessary references

--- Genetic Algorithm/Genetic Algorithm/GA/Generics/GeneticAlgorithm.cs
+++ Genetic Algorithm/Genetic Algorithm/GA/Generics/GeneticAlgorithm.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Genetic_Algorithm.Utils;
 
 namespace Genetic_Algorithm.GA.Generics
